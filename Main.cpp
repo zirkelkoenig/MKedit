9b4793b8518a6eaf653da0d1376ce0d63fa76752
Marvin Kipping <zirkelkoenig@users.noreply.github.com>
2022-09-26 21:39:47 +0200

    TTF: glyph index offsets parsed

--- Main.cpp
+++ Main.cpp
@@ -5,6 +5,12 @@
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
 
 struct FontTableRecord
 {
@@ -27,6 +33,14 @@ u32 BigBytesToU32(u8* bytes)
 u16 BigBytesToU16(u8* bytes)
 {
     u16 result = bytes[0u];
+    result <<= 8u;
+    result += bytes[1u];
+    return result;
+}
+
+i16 BigBytesToI16(u8* bytes)
+{
+    i16 result = bytes[0u];
     result <<= 8u;
     result += bytes[1u];
     return result;
@@ -61,7 +75,37 @@ void CopyFontFileBytesToBuffer(u32 count)
     ReadFile(FontFile, Buffer, count, &DiscardBytesRead, NULL);
 }
 
-u16* GlyphIds;
+FontTableRecord* FontTableRecords;
+u16 FontTableCount;
+
+u32 GetFontTableLocation(const char* tag)
+{
+    for (u32 i = 0u; i != FontTableCount; i++)
+    {
+        FontTableRecord record = FontTableRecords[i];
+        int tagsEqual =
+            record.Tag[0u] == tag[0u] &&
+            record.Tag[1u] == tag[1u] &&
+            record.Tag[2u] == tag[2u] &&
+            record.Tag[3u] == tag[3u];
+        if (tagsEqual)
+        {
+            return record.Offset;
+        }
+    }
+    return 0xFFFFFFFF;
+}
+
+int SetFontFilePointerToTable(const char* tag)
+{
+    u32 location = GetFontTableLocation(tag);
+    if (location == 0xFFFFFFFF)
+    {
+        return 0;
+    }
+    SetFilePointer(FontFile, location, NULL, FILE_BEGIN);
+    return 1;
+}
 
 int APIENTRY WinMain(
     HINSTANCE instance,
@@ -80,162 +124,229 @@ int APIENTRY WinMain(
 
     Heap = GetProcessHeap();
 
-    // u32 - sfntVersion
-    // u16 - tableCount
-    // u16 - searchRange
-    // u16 - entrySelector
-    // u16 - rangeShift
-    SetFilePointer(FontFile, sizeof(u32), NULL, FILE_BEGIN);
-    CopyFontFileBytesToBuffer(sizeof(u16));
-    u16 tableCount = BigBytesToU16(Buffer);
-    SetFilePointer(FontFile, 3u * sizeof(u16), NULL, FILE_CURRENT);
-
-    // 4 * u8 - tag
-    // u32 - checksum
-    // u32 - offset
-    // u32 - length
-    u32 tableRecordTagLength = 4u * sizeof(u8);
-    u32 tableRecordLength = (tableRecordTagLength + 3u * sizeof(u32));
-    CopyFontFileBytesToBuffer(tableCount * tableRecordLength);
-    FontTableRecord* tableRecords = (FontTableRecord*)HeapAlloc(Heap, 0u, tableCount * sizeof(FontTableRecord));
-    for (u32 i = 0u; i != tableCount; i++)
+    //--------------------
+    // Parse Main Table
     {
-        u32 offset = i * tableRecordLength;
-        FontTableRecord* tableRecord = &tableRecords[i];
-        CopyMemory(tableRecord->Tag, &Buffer[offset], tableRecordTagLength);
-        tableRecord->Offset = BigBytesToU32(&Buffer[offset + tableRecordTagLength + sizeof(u32)]);
-    }
+        // u32 - sfntVersion
+        // u16 - tableCount
+        // u16 - searchRange
+        // u16 - entrySelector
+        // u16 - rangeShift
+        SetFilePointer(FontFile, sizeof(u32), NULL, FILE_BEGIN);
+        CopyFontFileBytesToBuffer(sizeof(u16));
+        FontTableCount = BigBytesToU16(Buffer);
+        SetFilePointer(FontFile, 3u * sizeof(u16), NULL, FILE_CURRENT);
 
-    u32 cmapLocation = 0u;
-    for (u32 i = 0u; i != tableCount; i++)
-    {
-        FontTableRecord record = tableRecords[i];
-        int tagsEqual =
-            record.Tag[0u] == 'c' &&
-            record.Tag[1u] == 'm' &&
-            record.Tag[2u] == 'a' &&
-            record.Tag[3u] == 'p';
-        if (tagsEqual)
+        // 4 * u8 - tag
+        // u32 - checksum
+        // u32 - offset
+        // u32 - length
+        u32 tableRecordTagLength = 4u * sizeof(u8);
+        u32 tableRecordLength = (tableRecordTagLength + 3u * sizeof(u32));
+        CopyFontFileBytesToBuffer(FontTableCount * tableRecordLength);
+        FontTableRecords = (FontTableRecord*)HeapAlloc(Heap, 0u, FontTableCount * sizeof(FontTableRecord));
+        for (u32 i = 0u; i != FontTableCount; i++)
         {
-            cmapLocation = record.Offset;
-            break;
+            u32 offset = i * tableRecordLength;
+            FontTableRecord* tableRecord = &FontTableRecords[i];
+            CopyMemory(tableRecord->Tag, &Buffer[offset], tableRecordTagLength);
+            tableRecord->Offset = BigBytesToU32(&Buffer[offset + tableRecordTagLength + sizeof(u32)]);
         }
     }
-    SetFilePointer(FontFile, cmapLocation, NULL, FILE_BEGIN);
 
-    // u16 - version
-    // u16 - encodingRecordCount
-    SetFilePointer(FontFile, sizeof(u16), NULL, FILE_CURRENT);
-    CopyFontFileBytesToBuffer(sizeof(u16));
-    u16 encodingCount = BigBytesToU16(Buffer);
-
-    // u16 - platformId
-    // u16 - encodingId
-    // u32 - subtableOffset
-    u32 encodingRecordLength = 2u * sizeof(u16) + sizeof(u32);
-    CopyFontFileBytesToBuffer(encodingCount * encodingRecordLength);
-    u32 encodingOffset;
-    for (u32 i = 0u; i != encodingCount; i++)
+    //--------------------
+    // Parse Head Table
+    i16 locationOffsetType;
     {
-        u32 offset = i * encodingRecordLength;
-        u16 platformId = BigBytesToU16(&Buffer[offset]);
-        if (platformId == 0u) // Unicode
+        SetFontFilePointerToTable("head");
+
+        // u16 - majorVersion
+        // u16 - minorVersion
+        // i32 - fontRevision
+        // u32 - checksumAdjustment
+        // u32 - magicNumber
+        // u16 - flags
+        // u16 - unitsPerEm
+        // i64 - created
+        // i64 - modified
+        // i16 - xMin
+        // i16 - yMin
+        // i16 - xMax
+        // i16 - yMax
+        // u16 - macStyle
+        // u16 - lowestRecPpem
+        // i16 - fontDirectionHint
+        // i16 - indexToLocFormat
+        // i16 - glyphDataFormat
+        u32 skipBytes = 11u * sizeof(u16) + 3u * sizeof(u32) + 2u * sizeof(i64);
+        SetFilePointer(FontFile, skipBytes, NULL, FILE_CURRENT);
+        CopyFontFileBytesToBuffer(sizeof(u16));
+        locationOffsetType = BigBytesToI16(Buffer);
+    }
+
+    //---------------------------
+    // Parse Max Profile Table
+    u16 glyphCount;
+    {
+        SetFontFilePointerToTable("maxp");
+
+        // u32 - version
+        // u16 - numGlyphs
+        SetFilePointer(FontFile, sizeof(u32), NULL, FILE_CURRENT);
+        CopyFontFileBytesToBuffer(sizeof(u16));
+        glyphCount = BigBytesToU16(Buffer);
+    }
+
+    //-----------------------
+    // Parse Location Table
+    u32* glyphOffsets;
+    {
+        SetFontFilePointerToTable("loca");
+
+        u32 offsetSize = locationOffsetType ? sizeof(u32) : sizeof(u16);
+        u32 tableLength = glyphCount * offsetSize;
+        glyphOffsets = (u32*)HeapAlloc(Heap, 0u, tableLength);
+        CopyFontFileBytesToBuffer(tableLength);
+
+        if (locationOffsetType)
         {
-            u16 encodingId = BigBytesToU16(&Buffer[offset + sizeof(u16)]);
-            if (encodingId == 3u) // BMP
+            for (u32 i = 0u; i != glyphCount; i++)
             {
-                encodingOffset = BigBytesToU32(&Buffer[offset + 2u * sizeof(u16)]);
-                break;
+                u32 offset = i * offsetSize;
+                glyphOffsets[i] = BigBytesToU32(&Buffer[offset]);
+            }
+        }
+        else
+        {
+            for (u32 i = 0u; i != glyphCount; i++)
+            {
+                u32 offset = i * offsetSize;
+                glyphOffsets[i] = BigBytesToU16(&Buffer[offset]);
             }
         }
     }
-    SetFilePointer(FontFile, cmapLocation + encodingOffset, NULL, FILE_BEGIN);
 
-    // u16 - format
-    // u16 - length
-    // u16 - language
-    // u16 - segCountX2
-    // u16 - searchRange
-    // u16 - entrySelector
-    // u16 - rangeShift
-    SetFilePointer(FontFile, sizeof(u16), NULL, FILE_CURRENT);
-    CopyFontFileBytesToBuffer(sizeof(u16));
-    u16 encodingLength = BigBytesToU16(Buffer);
-    SetFilePointer(FontFile, sizeof(u16), NULL, FILE_CURRENT);
-    CopyFontFileBytesToBuffer(sizeof(u16));
-    u16 segmentCount = BigBytesToU16(Buffer) / 2u;
-    SetFilePointer(FontFile, 3u * sizeof(u16), NULL, FILE_CURRENT);
-
-    // segmentCount * u16 - endCodes
-    // u16 - reservedPad
-    // segmentCount * u16 - startCodes
-    // segmentCount * u16 - idDelta
-    // segmentCount * u16 - idRangeOffsets
-    u32 descriptorLength = (1u + 4u * segmentCount) * sizeof(u16);
-    CopyFontFileBytesToBuffer(descriptorLength);
-
-    u16* endCodes = (u16*)HeapAlloc(Heap, 0u, segmentCount * sizeof(u16));
-    for (u32 i = 0u; i != segmentCount; i++)
+    //-------------------
+    // Parse Mapping Table
+    u16* glyphIds;
     {
-        endCodes[i] = BigBytesToU16(&Buffer[i * sizeof(u16)]);
-    }
+        u32 tableLocation = GetFontTableLocation("cmap");
+        SetFilePointer(FontFile, tableLocation, NULL, FILE_BEGIN);
 
-    u32 offset = (segmentCount + 1u) * sizeof(u16);
-    u16* startCodes = (u16*)HeapAlloc(Heap, 0u, segmentCount * sizeof(u16));
-    for (u32 i = 0u; i != segmentCount; i++)
-    {
-        startCodes[i] = BigBytesToU16(&Buffer[offset + i * sizeof(u16)]);
-    }
+        // u16 - version
+        // u16 - encodingRecordCount
+        SetFilePointer(FontFile, sizeof(u16), NULL, FILE_CURRENT);
+        CopyFontFileBytesToBuffer(sizeof(u16));
+        u16 encodingCount = BigBytesToU16(Buffer);
 
-    offset += segmentCount * sizeof(u16);
-    u16* idDeltas = (u16*)HeapAlloc(Heap, 0u, segmentCount * sizeof(u16));
-    for (u32 i = 0u; i != segmentCount; i++)
-    {
-        idDeltas[i] = BigBytesToU16(&Buffer[offset + i * sizeof(u16)]);
-    }
-
-    offset += segmentCount * sizeof(u16);
-    u16* idRangeOffsets = (u16*)HeapAlloc(Heap, 0u, segmentCount * sizeof(u16));
-    for (u32 i = 0u; i != segmentCount; i++)
-    {
-        idRangeOffsets[i] = BigBytesToU16(&Buffer[offset + i * sizeof(u16)]) / sizeof(u16);
-    }
-
-    u32 glyphIndicesLength = encodingLength - (7u * sizeof(u16) + descriptorLength);
-    CopyFontFileBytesToBuffer(glyphIndicesLength);
-    u32 glyphIndicesCount = glyphIndicesLength / 2u;
-    u16* glyphIndices = (u16*)HeapAlloc(Heap, 0u, glyphIndicesLength);
-    for (u32 i = 0u; i != glyphIndicesCount; i++)
-    {
-        glyphIndices[i] = BigBytesToU16(&Buffer[i * sizeof(u16)]);
-    }
-
-    GlyphIds = (u16*)HeapAlloc(Heap, HEAP_ZERO_MEMORY, 0xFFFF * sizeof(u16));
-    for (u16 codepoint = 0x0000; codepoint != 0xFFFF; codepoint++)
-    {
-        u32 segment;
-        for (u32 i = 0u; i != segmentCount; i++)
+        // u16 - platformId
+        // u16 - encodingId
+        // u32 - subtableOffset
+        u32 encodingRecordLength = 2u * sizeof(u16) + sizeof(u32);
+        CopyFontFileBytesToBuffer(encodingCount * encodingRecordLength);
+        u32 encodingOffset;
+        for (u32 i = 0u; i != encodingCount; i++)
         {
-            if (endCodes[i] >= codepoint)
+            u32 offset = i * encodingRecordLength;
+            u16 platformId = BigBytesToU16(&Buffer[offset]);
+            if (platformId == 0u) // Unicode
             {
-                segment = i;
-                break;
-            }
-        }
-        if (startCodes[segment] <= codepoint)
-        {
-            if (idRangeOffsets[segment] != 0u)
-            {
-                u32 index = idRangeOffsets[segment] + (codepoint - startCodes[segment]) - (segmentCount - segment);
-                u32 glyphId = glyphIndices[index];
-                if (glyphId != 0)
+                u16 encodingId = BigBytesToU16(&Buffer[offset + sizeof(u16)]);
+                if (encodingId == 3u) // BMP
                 {
-                    GlyphIds[codepoint] = glyphId + idDeltas[segment] % 65536u;
+                    encodingOffset = BigBytesToU32(&Buffer[offset + 2u * sizeof(u16)]);
+                    break;
                 }
             }
-            else
+        }
+        SetFilePointer(FontFile, tableLocation + encodingOffset, NULL, FILE_BEGIN);
+
+        // u16 - format
+        // u16 - length
+        // u16 - language
+        // u16 - segCountX2
+        // u16 - searchRange
+        // u16 - entrySelector
+        // u16 - rangeShift
+        SetFilePointer(FontFile, sizeof(u16), NULL, FILE_CURRENT);
+        CopyFontFileBytesToBuffer(sizeof(u16));
+        u16 encodingLength = BigBytesToU16(Buffer);
+        SetFilePointer(FontFile, sizeof(u16), NULL, FILE_CURRENT);
+        CopyFontFileBytesToBuffer(sizeof(u16));
+        u16 segmentCount = BigBytesToU16(Buffer) / 2u;
+        SetFilePointer(FontFile, 3u * sizeof(u16), NULL, FILE_CURRENT);
+
+        // segmentCount * u16 - endCodes
+        // u16 - reservedPad
+        // segmentCount * u16 - startCodes
+        // segmentCount * u16 - idDelta
+        // segmentCount * u16 - idRangeOffsets
+        u32 descriptorLength = (1u + 4u * segmentCount) * sizeof(u16);
+        CopyFontFileBytesToBuffer(descriptorLength);
+
+        u16* endCodes = (u16*)HeapAlloc(Heap, 0u, segmentCount * sizeof(u16));
+        for (u32 i = 0u; i != segmentCount; i++)
+        {
+            endCodes[i] = BigBytesToU16(&Buffer[i * sizeof(u16)]);
+        }
+
+        u32 offset = (segmentCount + 1u) * sizeof(u16);
+        u16* startCodes = (u16*)HeapAlloc(Heap, 0u, segmentCount * sizeof(u16));
+        for (u32 i = 0u; i != segmentCount; i++)
+        {
+            startCodes[i] = BigBytesToU16(&Buffer[offset + i * sizeof(u16)]);
+        }
+
+        offset += segmentCount * sizeof(u16);
+        u16* idDeltas = (u16*)HeapAlloc(Heap, 0u, segmentCount * sizeof(u16));
+        for (u32 i = 0u; i != segmentCount; i++)
+        {
+            idDeltas[i] = BigBytesToU16(&Buffer[offset + i * sizeof(u16)]);
+        }
+
+        offset += segmentCount * sizeof(u16);
+        u16* idRangeOffsets = (u16*)HeapAlloc(Heap, 0u, segmentCount * sizeof(u16));
+        for (u32 i = 0u; i != segmentCount; i++)
+        {
+            idRangeOffsets[i] = BigBytesToU16(&Buffer[offset + i * sizeof(u16)]) / sizeof(u16);
+        }
+
+        u32 glyphIndicesLength = encodingLength - (7u * sizeof(u16) + descriptorLength);
+        CopyFontFileBytesToBuffer(glyphIndicesLength);
+        u32 glyphIndicesCount = glyphIndicesLength / 2u;
+        u16* glyphIndices = (u16*)HeapAlloc(Heap, 0u, glyphIndicesLength);
+        for (u32 i = 0u; i != glyphIndicesCount; i++)
+        {
+            glyphIndices[i] = BigBytesToU16(&Buffer[i * sizeof(u16)]);
+        }
+
+        glyphIds = (u16*)HeapAlloc(Heap, HEAP_ZERO_MEMORY, 0xFFFF * sizeof(u16));
+        for (u16 codepoint = 0x0000; codepoint != 0xFFFF; codepoint++)
+        {
+            u32 segment;
+            for (u32 i = 0u; i != segmentCount; i++)
             {
-                GlyphIds[codepoint] = (u32)codepoint + idDeltas[segment] % 65536u;
+                if (endCodes[i] >= codepoint)
+                {
+                    segment = i;
+                    break;
+                }
+            }
+            if (startCodes[segment] <= codepoint)
+            {
+                if (idRangeOffsets[segment] != 0u)
+                {
+                    u32 index = idRangeOffsets[segment] + (codepoint - startCodes[segment]) - (segmentCount - segment);
+                    u32 glyphId = glyphIndices[index];
+                    if (glyphId != 0)
+                    {
+                        glyphIds[codepoint] = glyphId + idDeltas[segment] % 65536u;
+                    }
+                }
+                else
+                {
+                    glyphIds[codepoint] = (u32)codepoint + idDeltas[segment] % 65536u;
+                }
             }
         }
     }
